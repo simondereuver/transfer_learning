4f4b3f7991b3d5ef63be4d6bd160ff86de32ece0
SimondeReuver <dereuversimon@gmail.com>
2025-04-02 21:39:51 +0200

    Added code to replace last layer of stanford model to fit for cats vs dogs dataset

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ if __name__ == "__main__":
     #path_to_images = "kagglecatsanddogs_5340" #uncomment and adjust path if needed
 
     #filter out corrupted images
-    dm.filter_images(path="data/stanford_dogs/Images") #uncomment if running first time
+    #dm.filter_images() #uncomment if running first time
     
     IMAGE_SIZE = (180, 180)
     BATCH_SIZE = 128
     NUM_CLASSES_STANFORD_DOGS = 120
     NUM_CLASSES_CATS_DOGS = 2
 
-    train_ds, val_ds = dm.train_val_split(image_size=IMAGE_SIZE, batch_size=BATCH_SIZE, path="stanford_dogs/Images")
+    # TODO: fix train_val_split to also create a test split so we can evaluate models
+    train_ds, val_ds = dm.train_val_split(image_size=IMAGE_SIZE, batch_size=BATCH_SIZE)
 
-    dm.vis_data(train_ds)
+    #dm.vis_data(train_ds)
 
     #set how you want to augment the data
     data_augmentation_layers = [
@@ -36,8 +37,51 @@ if __name__ == "__main__":
     train_ds = train_ds.prefetch(tf_data.AUTOTUNE)
     val_ds = val_ds.prefetch(tf_data.AUTOTUNE)
 
-    dm.vis_data_augmentations(train_ds, data_augmentation_layers)
+    #dm.vis_data_augmentations(train_ds, data_augmentation_layers)
 
+    ##### EXPERIMENTS #####
+
+    EPOCHS=50
+    LR=1e-4
+    
+    #experiment 2: load the saved model and replace only the output layer of the model (to align it to the new problem). 
+
+    #load model
+    stanford_model = keras.models.load_model("trained_models/stanford_dogs_epoch_50.keras") #base model
+    #stanford_model.summary() #useful to see what current model looks like
+
+    #get output before last layer
+    x = stanford_model.get_layer('dropout').output
+    
+    #connect to output layer
+    new_output = layers.Dense(NUM_CLASSES_CATS_DOGS, activation=None)(x)
+
+    #create the new model
+    ex2_model = keras.Model(inputs=stanford_model.input, outputs=new_output)
+
+    #compare summaries to make sure last layer was replaced as intended
+    #stanford_model.summary()
+    #ex2_model.summary()
+
+    #train and evaluate the model (for 50 epochs) on the cats and dogs dataset.
+
+    ex2_model.compile(
+        optimizer=keras.optimizers.Adam(LR),
+        loss=keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=[keras.metrics.BinaryAccuracy()],
+    )
+    callbacks = [
+        keras.callbacks.ModelCheckpoint("trained_models/ex2_model_{epoch}.keras"),
+    ]
+
+    ex2_model.fit(
+        train_ds,
+        epochs=EPOCHS,
+        callbacks=callbacks,
+        validation_data=val_ds,
+    )
+
+    """
     model = custom_model.make_model(input_shape=IMAGE_SIZE + (3,), num_classes=NUM_CLASSES_STANFORD_DOGS)
     keras.utils.plot_model(model, show_shapes=True)
 
@@ -56,3 +100,4 @@ if __name__ == "__main__":
 
     #load model like this:
     #stanford_model = keras.models.load_model("stanford_dogs_model.h5")
+    """
