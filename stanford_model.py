5f237104f6fddf17601462f9a77c0d8b9f421fc4
Ivo Nilsson <ivo_gote@hotmail.com>
2025-04-03 09:31:38 +0200

    New function, train_val_test_split. Rearranged the imports to make it easier to run on the compute server.

--- stanford_model.py
+++ stanford_model.py
@@ -6,27 +6,23 @@ Stores the model during epochs and when completed training.
 import datamodule as dm
 import model as custom_model
 from tensorflow import data as tf_data
-import keras
-from keras import layers
+import tensorflow.keras as keras
+from tensorflow.keras import layers
 
 IMAGE_SIZE=(180, 180)
-BATCH_SIZE=128
+BATCH_SIZE=64
 VAL_SPLIT=0.2
 EPOCHS=50
 LR=1e-4
+NUM_CLASSES_STANFORD_DOGS = 120
 
 #load the stanford dataset
 #data path: data/stanford_dogs/Images
-dm.filter_images(path="data/stanford_dogs/Images") #uncomment if running first time
-
-IMAGE_SIZE = (180, 180)
-BATCH_SIZE = 128
-NUM_CLASSES_STANFORD_DOGS = 120
-
+#dm.filter_images(path="data/stanford_dogs/Images") #uncomment if running first time
 
 train_ds, val_ds = dm.train_val_split(image_size=IMAGE_SIZE, batch_size=BATCH_SIZE, path="data/stanford_dogs/Images")
 
-dm.vis_data(train_ds)
+#dm.vis_data(train_ds)
 
 #set how you want to augment the data
 data_augmentation_layers = [
@@ -43,10 +39,10 @@ train_ds = train_ds.map(
 train_ds = train_ds.prefetch(tf_data.AUTOTUNE)
 val_ds = val_ds.prefetch(tf_data.AUTOTUNE)
 
-dm.vis_data_augmentations(train_ds, data_augmentation_layers)
+#dm.vis_data_augmentations(train_ds, data_augmentation_layers)
 
 model = custom_model.make_model(input_shape=IMAGE_SIZE + (3,), num_classes=NUM_CLASSES_STANFORD_DOGS)
-keras.utils.plot_model(model, show_shapes=True)
+#keras.utils.plot_model(model, show_shapes=True)
 
 #train a model based on stanford dataset
 history = custom_model.train_model(
